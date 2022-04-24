c6c752f4cfa541579b0079e57c7e7a58ce8a98ce
psuen1811 <psuen1811@gmail.com>
2022-04-23 22:49:49 -0700

    Used switch case

--- NobleSearchWS/src/main/java/com/pakfortune/model/star/Horse.java
+++ NobleSearchWS/src/main/java/com/pakfortune/model/star/Horse.java
@@ -19,56 +19,59 @@ public class Horse implements GetResultByStemBranch, Star {
     @Override
     public SixtyJiaziTable checkStemBranch(String stem, String branch) {
         SixtyJiaziTable sixtyJiaziTable = null;
-        if( SOUTH.contains(branch))
-        {
-            if (stem.equals("甲"))
-                sixtyJiaziTable = SixtyJiaziTable.壬申;
-            if (stem.equals("丙"))
-                sixtyJiaziTable = SixtyJiaziTable.丙申;
-            if (stem.equals("戊"))
-                sixtyJiaziTable = SixtyJiaziTable.庚申;
-            if (stem.equals("庚"))
-                sixtyJiaziTable = SixtyJiaziTable.甲申;
-            if (stem.equals("壬"))
-                sixtyJiaziTable = SixtyJiaziTable.戊申;
-        }
-        else if( NORTH.contains(branch) ) {
-            if (stem.equals("甲"))
-                sixtyJiaziTable = SixtyJiaziTable.丙寅;
-            if (stem.equals("丙"))
-                sixtyJiaziTable = SixtyJiaziTable.庚寅;
-            if (stem.equals("戊"))
-                sixtyJiaziTable = SixtyJiaziTable.甲寅;
-            if (stem.equals("庚"))
-                sixtyJiaziTable = SixtyJiaziTable.戊寅;
-            if (stem.equals("壬"))
-                sixtyJiaziTable = SixtyJiaziTable.壬寅;
-        }
-        else if( EAST.contains(branch) ) {
-            if (stem.equals("乙"))
-                sixtyJiaziTable = SixtyJiaziTable.辛巳;
-            if (stem.equals("丁"))
-                sixtyJiaziTable = SixtyJiaziTable.乙巳;
-            if (stem.equals("己"))
-                sixtyJiaziTable = SixtyJiaziTable.己巳;
-            if (stem.equals("辛"))
-                sixtyJiaziTable = SixtyJiaziTable.癸巳;
-            if (stem.equals("癸"))
-                sixtyJiaziTable = SixtyJiaziTable.丁巳;
-        }
-        else if( WEST.contains(branch) ) {
-            if (stem.equals("乙"))
-                sixtyJiaziTable = SixtyJiaziTable.丁亥;
-            if (stem.equals("丁"))
-                sixtyJiaziTable = SixtyJiaziTable.辛亥;
-            if (stem.equals("己"))
-                sixtyJiaziTable = SixtyJiaziTable.乙亥;
-            if (stem.equals("辛"))
-                sixtyJiaziTable = SixtyJiaziTable.己亥;
-            if (stem.equals("癸"))
-                sixtyJiaziTable = SixtyJiaziTable.癸亥;
+        if (SOUTH.contains(branch)) {
+            switch (stem) {
+                case "甲":
+                    sixtyJiaziTable = SixtyJiaziTable.壬申;
+                case "丙":
+                    sixtyJiaziTable = SixtyJiaziTable.丙申;
+                case "戊":
+                    sixtyJiaziTable = SixtyJiaziTable.庚申;
+                case "庚":
+                    sixtyJiaziTable = SixtyJiaziTable.甲申;
+                case "壬":
+                    sixtyJiaziTable = SixtyJiaziTable.戊申;
+            }
+        } else if (NORTH.contains(branch)) {
+            switch (stem) {
+                case "甲":
+                    sixtyJiaziTable = SixtyJiaziTable.丙寅;
+                case "丙":
+                    sixtyJiaziTable = SixtyJiaziTable.庚寅;
+                case "戊":
+                    sixtyJiaziTable = SixtyJiaziTable.甲寅;
+                case "庚":
+                    sixtyJiaziTable = SixtyJiaziTable.戊寅;
+                case "壬":
+                    sixtyJiaziTable = SixtyJiaziTable.壬寅;
+            }
+        } else if (EAST.contains(branch)) {
+            switch (stem) {
+                case "乙":
+                    sixtyJiaziTable = SixtyJiaziTable.辛巳;
+                case "丁":
+                    sixtyJiaziTable = SixtyJiaziTable.乙巳;
+                case "己":
+                    sixtyJiaziTable = SixtyJiaziTable.己巳;
+                case "辛":
+                    sixtyJiaziTable = SixtyJiaziTable.癸巳;
+                case "癸":
+                    sixtyJiaziTable = SixtyJiaziTable.丁巳;
+            }
+        } else if (WEST.contains(branch)) {
+            switch (stem) {
+                case "乙":
+                    sixtyJiaziTable = SixtyJiaziTable.丁亥;
+                case "丁":
+                    sixtyJiaziTable = SixtyJiaziTable.辛亥;
+                case "己":
+                    sixtyJiaziTable = SixtyJiaziTable.乙亥;
+                case "辛":
+                    sixtyJiaziTable = SixtyJiaziTable.己亥;
+                case "癸":
+                    sixtyJiaziTable = SixtyJiaziTable.癸亥;
+            }
         }
         return sixtyJiaziTable;
     }
-
 }
