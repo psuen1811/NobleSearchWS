463db6119d485088500cf132dbc1323982ac4f8c
psuen1811 <psuen1811@gmail.com>
2022-04-23 22:56:59 -0700

    Fixed bugs

--- NobleSearchWS/src/main/java/com/pakfortune/model/star/Horse.java
+++ NobleSearchWS/src/main/java/com/pakfortune/model/star/Horse.java
@@ -23,53 +23,73 @@ public class Horse implements GetResultByStemBranch, Star {
             switch (stem) {
                 case "甲":
                     sixtyJiaziTable = SixtyJiaziTable.壬申;
+                    break;
                 case "丙":
                     sixtyJiaziTable = SixtyJiaziTable.丙申;
+                    break;
                 case "戊":
                     sixtyJiaziTable = SixtyJiaziTable.庚申;
+                    break;
                 case "庚":
                     sixtyJiaziTable = SixtyJiaziTable.甲申;
+                    break;
                 case "壬":
                     sixtyJiaziTable = SixtyJiaziTable.戊申;
+                    break;
             }
         } else if (NORTH.contains(branch)) {
             switch (stem) {
                 case "甲":
                     sixtyJiaziTable = SixtyJiaziTable.丙寅;
+                    break;
                 case "丙":
                     sixtyJiaziTable = SixtyJiaziTable.庚寅;
+                    break;
                 case "戊":
                     sixtyJiaziTable = SixtyJiaziTable.甲寅;
+                    break;
                 case "庚":
                     sixtyJiaziTable = SixtyJiaziTable.戊寅;
+                    break;
                 case "壬":
                     sixtyJiaziTable = SixtyJiaziTable.壬寅;
+                    break;
             }
         } else if (EAST.contains(branch)) {
             switch (stem) {
                 case "乙":
                     sixtyJiaziTable = SixtyJiaziTable.辛巳;
+                    break;
                 case "丁":
                     sixtyJiaziTable = SixtyJiaziTable.乙巳;
+                    break;
                 case "己":
                     sixtyJiaziTable = SixtyJiaziTable.己巳;
+                    break;
                 case "辛":
                     sixtyJiaziTable = SixtyJiaziTable.癸巳;
+                    break;
                 case "癸":
                     sixtyJiaziTable = SixtyJiaziTable.丁巳;
+                    break;
             }
         } else if (WEST.contains(branch)) {
             switch (stem) {
                 case "乙":
                     sixtyJiaziTable = SixtyJiaziTable.丁亥;
+                    break;
                 case "丁":
                     sixtyJiaziTable = SixtyJiaziTable.辛亥;
+                    break;
                 case "己":
                     sixtyJiaziTable = SixtyJiaziTable.乙亥;
+                    break;
                 case "辛":
                     sixtyJiaziTable = SixtyJiaziTable.己亥;
+                    break;
                 case "癸":
                     sixtyJiaziTable = SixtyJiaziTable.癸亥;
+                    break;
             }
         }
         return sixtyJiaziTable;
