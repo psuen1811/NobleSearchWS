5dc12579b64f64befbc2f9767494da3ef1c78414
psuen1811 <psuen1811@gmail.com>
2023-03-05 21:53:07 -0800

    Changed to abstract class

--- NobleSearchWS/src/main/java/com/pakfortune/common/FiveTigerHop.java
+++ NobleSearchWS/src/main/java/com/pakfortune/common/FiveTigerHop.java
@@ -3,7 +3,7 @@ package com.pakfortune.common;
 import com.pakfortune.model.element.Stem;
 
 // 五虎遁
-public class FiveTigerHop extends LookupImpl {
+public abstract class FiveTigerHop extends LookupImpl {
     public static Stem hop(String input) {
         String targetStem = switch (input) {
             case "甲", "己" -> "丙";
