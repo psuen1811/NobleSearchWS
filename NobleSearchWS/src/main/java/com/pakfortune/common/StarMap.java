951b4887614d7cc17b6ca63c685ebf5cd4871a4c
psuen1811 <psuen1811@gmail.com>
2023-03-28 11:20:03 -0700

    Added extends Star

--- NobleSearchWS/src/main/java/com/pakfortune/common/StarMap.java
+++ NobleSearchWS/src/main/java/com/pakfortune/common/StarMap.java
@@ -2,7 +2,7 @@ package com.pakfortune.common;
 
 import com.google.common.collect.ImmutableMap;
 
-public interface StarMap {
+public interface StarMap extends Star {
 
     default <K, V> String lookupMap(K input, ImmutableMap<K, V> mapper) {
         return mapper.get(input).toString();
