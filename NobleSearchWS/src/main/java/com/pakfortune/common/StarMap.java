6c343e6264db5c74d579e5d6bb797e99bbc2ed64
psuen1811 <psuen1811@gmail.com>
2023-06-17 13:51:21 -0700

    Implemented null-checking

--- NobleSearchWS/src/main/java/com/pakfortune/common/StarMap.java
+++ NobleSearchWS/src/main/java/com/pakfortune/common/StarMap.java
@@ -5,6 +5,8 @@ import com.google.common.collect.ImmutableMap;
 public interface StarMap extends Star {
 
     default <K, V> String lookupMap(K input, ImmutableMap<K, V> mapper) {
-        return mapper.get(input).toString();
+        V value = mapper.get(input);
+        return value != null ? value.toString() : null;
     }
 }
+
