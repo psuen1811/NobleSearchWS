ae0d9e1ad94e2a0bea7108b9ec60d16b210fa56e
psuen1811 <psuen1811@gmail.com>
2023-06-19 11:26:45 -0700

    Inserted comments

--- NobleSearchWS/src/main/java/com/pakfortune/service/NobleSearchService.java
+++ NobleSearchWS/src/main/java/com/pakfortune/service/NobleSearchService.java
@@ -10,8 +10,18 @@ import java.util.List;
 
 @SuppressWarnings("all")
 @Component
+/**
+ * This class is a service class responsible for calculating the noble persons in Chinese astrology.
+ * It encapsulates the methods of calculating the celestial stems and branches based on the input.
+ */
 public class NobleSearchService {
 
+    /**
+     * This method calculates the noble persons based on the input string.
+     *
+     * @param input A string that represents the stem and branch.
+     * @return A StringBuilder containing the results of the calculation.
+     */
     public StringBuilder getNobles(String input) {
         StringBuilder stringBuilder = new StringBuilder("-------真祿馬貴人-------\n\n");
         // declare 60 lunar "六十甲子"" by sequence
@@ -28,39 +38,39 @@ public class NobleSearchService {
             String tempStem = stemBranchArray[0];
             String tempBranch = stemBranchArray[1];
 
-            // 根據輸入干支飛遁六十甲子
+            // Based on the input stem and branch, rotate the Sixty Jiazi Table.
             circularArrayList.shiftRight(LookupUtils.getIfPresent(SIXTY_JIAZI_TABLE.class, input).ordinal());
             String result;
 
-            // 找真祿干支
+            // Find the stem and branch for Money
             Money money = new Money();
             result = money.lookupMap(tempStem, money.getMap());
             stringBuilder.append(NoblesOutput.calculateAndPrint(result, circularArrayList, money.getName())).append("\n");
 
-            // 找真馬干支
+            // Find the stem and branch for Horse
             Horse horseResult = new Horse();
             result = horseResult.checkStemBranch(tempStem, tempBranch).toString();
             stringBuilder.append(NoblesOutput.calculateAndPrint(result, circularArrayList, horseResult.getName())).append("\n");
 
-            // 找貴人干支
+            // Find the stem and branch for Rich Man
             RichMan richman = new RichMan();
             List<String> richmanList = richman.lookupList(tempStem);
             for (String s : richmanList) {
                 stringBuilder.append(NoblesOutput.calculateAndPrint(s, circularArrayList, richman.getName())).append("\n");
             }
 
-            // 真文昌
+            // Find the stem and branch for Study
             Study study = new Study();
             result = study.lookupMap(tempStem, study.getMap());
             stringBuilder.append(NoblesOutput.calculateAndPrint(result, circularArrayList, study.getName())).append("\n");
 
-            // 紅鸞
+            // Find the stem and branch for RedFlower
             RedFlower redFlower = new RedFlower();
             result = redFlower.lookupMap(tempBranch, redFlower.getMap());
             result = GetBranchByStem.calculate(tempStem, result);
             stringBuilder.append(NoblesOutput.calculateAndPrint(result, circularArrayList, redFlower.getName())).append("\n");
 
-            // 天喜
+            // Find the stem and branch for SkyHappiness
             SkyHappiness skyHappiness = new SkyHappiness();
             result = skyHappiness.lookupMap(tempBranch, skyHappiness.getMap());
             result = GetBranchByStem.calculate(tempStem, result);
@@ -69,3 +79,4 @@ public class NobleSearchService {
         return stringBuilder;
     }
 }
+
