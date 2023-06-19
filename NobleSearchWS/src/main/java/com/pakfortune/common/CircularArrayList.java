9b1921756e59e87ba5182949c30e8d1be5f0d5d9
psuen1811 <psuen1811@gmail.com>
2023-06-19 11:05:39 -0700

    Used the Collections.rotate() method for shifting elements

--- NobleSearchWS/src/main/java/com/pakfortune/common/CircularArrayList.java
+++ NobleSearchWS/src/main/java/com/pakfortune/common/CircularArrayList.java
@@ -1,52 +1,56 @@
 package com.pakfortune.common;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-@SuppressWarnings("serial")
+/*
+  This class provides a circular version of an ArrayList,
+  which can be rotated to the right or left with specified steps.
+
+  @param <E> the type of elements in this list
+ */
 public class CircularArrayList<E> extends ArrayList<E> {
+    /**
+     * Constructs an empty list with an initial capacity of ten.
+     */
     public CircularArrayList() {
+        super();
     }
 
+    /**
+     * Constructs an empty list with the specified initial capacity.
+     *
+     * @param size the initial capacity of the list
+     */
     public CircularArrayList(int size) {
         super(size);
     }
 
+    /**
+     * Constructs a list containing the elements of the specified collection,
+     * in the order they are returned by the collection's iterator.
+     *
+     * @param list the collection whose elements are to be placed into this list
+     */
     public CircularArrayList(final List<E> list) {
         super(list);
     }
 
+    /**
+     * Shifts the elements in the list to the right by a specified number of positions.
+     *
+     * @param shiftSize the number of positions to shift the elements
+     */
     public void shiftRight(int shiftSize) {
-        int size = this.size();
-
-        Map<Integer, E> temp = new HashMap<>();
-
-        int i = 0;
-        while (i < size) {
-            int k = (i + shiftSize) % size;
-
-            temp.put(k, this.get(i));
-            i++;
-        }
-
-        this.clear();
-        this.addAll(temp.values());
+        Collections.rotate(this, shiftSize);
     }
 
+    /**
+     * Shifts the elements in the list to the left by a specified number of positions.
+     *
+     * @param shiftSize the number of positions to shift the elements
+     */
     public void shiftLeft(int shiftSize) {
-        int size = this.size();
-        List<E> temp = new ArrayList<>();
-
-        int i = 0;
-        while (i < size) {
-            int k = (i + shiftSize) % size;
-            temp.add(this.get(k));
-            i++;
-        }
-
-        this.clear();
-        this.addAll(temp);
+        Collections.rotate(this, -shiftSize);
     }
 }
+
