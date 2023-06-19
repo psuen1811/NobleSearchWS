40f84c0632ac3657ac0cb3a681fff040de1dd5ff
psuen1811 <psuen1811@gmail.com>
2023-06-19 11:20:13 -0700

    Inserted comments

--- NobleSearchWS/src/main/java/com/pakfortune/common/FiveTigerHop.java
+++ NobleSearchWS/src/main/java/com/pakfortune/common/FiveTigerHop.java
@@ -2,8 +2,19 @@ package com.pakfortune.common;
 
 import com.pakfortune.model.element.Stem;
 
-// 五虎遁
+/**
+ * This is an abstract class representing the "Five Tiger Escape" (五虎遁) concept in Chinese metaphysics.
+ * The method hop() is used to get the "escaped" heavenly stem based on the input stem.
+ */
 public abstract class FiveTigerHop {
+
+    /**
+     * Returns the "escaped" stem according to the input stem based on the rules of "Five Tiger Escape" (五虎遁).
+     *
+     * @param input The input heavenly stem as a String.
+     * @return The "escaped" stem as an object of enum Stem.
+     * @throws IllegalArgumentException if the input string is not a valid heavenly stem.
+     */
     public static Stem hop(String input) {
         String targetStem = switch (input) {
             case "甲", "己" -> "丙";
@@ -17,4 +28,5 @@ public abstract class FiveTigerHop {
         return LookupUtils.getIfPresent(Stem.class, targetStem);
     }
 }
+
 
