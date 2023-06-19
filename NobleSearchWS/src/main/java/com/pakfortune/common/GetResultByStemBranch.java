62c4f1e4e7c72a3a3f79d3cee54227b822eca2f9
psuen1811 <psuen1811@gmail.com>
2023-06-19 11:17:32 -0700

    Inserted comments

--- NobleSearchWS/src/main/java/com/pakfortune/common/GetResultByStemBranch.java
+++ NobleSearchWS/src/main/java/com/pakfortune/common/GetResultByStemBranch.java
@@ -2,6 +2,20 @@ package com.pakfortune.common;
 
 import com.pakfortune.model.element.SIXTY_JIAZI_TABLE;
 
+/**
+ * An interface for classes that can check and process stem-branch pairs.
+ * Classes implementing this interface can provide a way to match the combination
+ * of a stem and a branch to a particular value in the SIXTY_JIAZI_TABLE.
+ */
 public interface GetResultByStemBranch {
+    /**
+     * Checks the combination of a given stem and branch and matches it to a
+     * particular value in the SIXTY_JIAZI_TABLE.
+     *
+     * @param stem   The stem part of a stem-branch pair.
+     * @param branch The branch part of a stem-branch pair.
+     * @return The corresponding SIXTY_JIAZI_TABLE value for the given stem and branch.
+     */
     SIXTY_JIAZI_TABLE checkStemBranch(String stem, String branch);
 }
+
