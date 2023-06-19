640d08c5f567c1942c86bd7dabd3358abe685428
psuen1811 <psuen1811@gmail.com>
2023-06-19 11:18:52 -0700

    Inserted comments

--- NobleSearchWS/src/main/java/com/pakfortune/common/StarList.java
+++ NobleSearchWS/src/main/java/com/pakfortune/common/StarList.java
@@ -2,6 +2,19 @@ package com.pakfortune.common;
 
 import java.util.List;
 
+/**
+ * An interface for classes that can look up a list based on a string input, while
+ * also extending the Star interface.
+ * Implementations of this interface represent entities that associate a list
+ * of strings with a certain Star input.
+ */
 public interface StarList extends Star {
+    /**
+     * Looks up a list of strings associated with the provided input.
+     *
+     * @param input The string input to look up.
+     * @return The list of strings associated with the input.
+     */
     List<String> lookupList(String input);
 }
+
